0b4e148a569d0035d172eedca66c30823fcfe498
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-26 11:10:50 +0200

    äntligen vg

--- A1/js/app.js
+++ A1/js/app.js
@@ -1,19 +1,10 @@
-const button0 = document.getElementById("button0");
-button0.addEventListener("click", function() {
-  randomNumberPlayer();
-  showRandomNumberPlayer();
-  randomNumberAi();
-  showRandomNumberAi();
-  resultOfRoll();
-  updateScore();
-})
-
 //DÄR PLAYERS KAST VISAS
 const playerRollTextfield = document.getElementById("playerRollText");
 const aiRollTextfield = document.getElementById("aiRollText");
 const resultTextfield = document.getElementById("result");
 const winsLoosesPlayerTextfield = document.getElementById("winsLoosesPlayer");
 const winsLoosesAiTextfield = document.getElementById("winsLoosesAi");
+
 
 
 let playerRoll = 0 ;
@@ -23,10 +14,25 @@ let aiScore = 0;        //den totala Scoren för ai
 
 
 //Controllers
+
+loadScore();
+updateScore();
+
+const button0 = document.getElementById("button0");
+button0.addEventListener("click", function() {
+  randomNumberPlayer();
+  showRandomNumberPlayer();
+  randomNumberAi();
+  showRandomNumberAi();
+  resultOfRoll();
+  updateScore();
+  saveScore();
+});
+
+
 //FUNKTIONER FÖR PLAYER
 function randomNumberPlayer() {
  playerRoll = Math.floor(Math.random() *6) +1;
-
 }
 
 function showRandomNumberPlayer() {
@@ -50,27 +56,53 @@ function showRandomNumberAi() {
 //funktionens namn + de värden som behövs för att utföra funktionen
 
 function resultOfRoll () {
-
   //här vill jag också lägga till att när detta händer så blir det +1 på resp. score
   if (playerRoll > aiRoll) {
     playerScore++;
-    return resultTextfield.innerHTML = "Player Wins! ";
+    return resultTextfield.innerHTML = "Player Wins! 👏🏼 ";
   }
 
-  else if
-  (playerRoll < aiRoll) {
+  else if (playerRoll < aiRoll) {
     aiScore++;
-    return resultTextfield.innerHTML = "Computer Win!";
+    return resultTextfield.innerHTML = "Computer Win! 👏🏼 ";
   }
 
-  else
-  {
-    return resultTextfield.innerHTML = "Draw!";
+  else {
+    return resultTextfield.innerHTML = "Draw! 🤷🏼‍♀️ ";
   }
 }
 
 
 function updateScore() {
-  winsLoosesPlayerTextfield.innerHTML = "Total score of player : " + playerScore;
-  winsLoosesAiTextfield.innerHTML = "Total score of ai : " + aiScore;
+  winsLoosesPlayerTextfield.innerHTML = "Total score of Player : " + playerScore;
+  winsLoosesAiTextfield.innerHTML = "Total score of Computer : " + aiScore;
+}
+
+
+//COOKIES   SKAPA EN FUNKTION FÖR ATT SPARA RESULTATEN I EN COOKIE
+function saveScore() {
+  document.cookie="previousPlayerScores="+playerScore+";expires= Fri, 27 Sep 2024 00.00:00 UTC";
+  document.cookie="previousAiScores="+aiScore+";expires= Fri, 27 Sep 2024 00.00:00 UTC";
+}
+
+
+function loadScore() {
+  //game load from 2 cookies
+  playerScore = getCookie("previousPlayerScores");
+  aiScore = getCookie("previousAiScores");
+}
+
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }return 0;
 }
